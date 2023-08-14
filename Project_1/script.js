bda30266a52e231afbd71440cea6e0bff5d12027
Drimesh <69688465+Drimeshpendam@users.noreply.github.com>
2023-08-15 01:57:20 +0530

    Revert "Submit Code"
    This reverts commit 208b5a037a04238e29b032f7289195b35da24569.

--- Project_1/script.js
+++
@@ -1,9 +0,0 @@
-const flagContainer = document.querySelector('.flag-container');
-
-flagContainer.addEventListener('mouseover', () => {
-  flagContainer.classList.add('waving');
-});
-
-flagContainer.addEventListener('mouseout', () => {
-  flagContainer.classList.remove('waving');
-});
